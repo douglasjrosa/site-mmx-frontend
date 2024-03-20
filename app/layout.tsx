96a2d41520f52f330def035cfd343764ea863e42
Douglas <douglasjrosa@gmail.com>
2024-03-20 14:41:32 -0300

    .gitignore donne

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
-import type { Viewport } from "next";
 import {
   siteInfo,
   robots,
