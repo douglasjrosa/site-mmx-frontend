a08673b2ea62d5a2f4d6caa3d45c08c106a4c352
douglasjrosa <63507739+douglasjrosa@users.noreply.github.com>
2024-04-03 17:58:09 -0300

    Testing api

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,33 @@
 import { baseUrl } from "@/data/global";
 
 export const getPost = async (slug: string) => {
-	let res = await fetch(`${baseUrl}/api/${slug}`, { method: "GET" }).then((r) => r.json());
-	if (res.post === undefined) return undefined;
-	const { post } = res;
-	return post;
+	try {
+		let res = await fetch(`${baseUrl}/api/${slug}`, {
+			method: "GET",
+			headers: {
+				'Accept': 'application/json'
+			}
+		}).then((data) => data.json());
+		if (res.post === undefined) return undefined;
+		const { post } = res;
+		return post;
+	} catch (error) {
+		console.error('Erro ao fazer a solicitação:', error);
+	}
 }
 
 export const getPosts = async () => {
-	let res = await fetch(`${baseUrl}/api/posts`, { method: "GET" }).then((r) => r.json());
-	if (res.posts === undefined) return undefined;
-	const { posts } = res;
-	return posts;
+	try {
+		let res = await fetch(`${baseUrl}/api/posts`, {
+			method: "GET",
+			headers: {
+				'Accept': 'application/json'
+			}
+		}).then((data) => data.json());
+		if (res.posts === undefined) return undefined;
+		const { posts } = res;
+		return posts;
+	} catch (error) {
+		console.error('Erro ao fazer a solicitação:', error);
+	}
 }
