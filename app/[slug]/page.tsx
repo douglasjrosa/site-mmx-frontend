edd4201e74ca821f8f6e5a64673b2d9fec8e8bb2
douglasjrosa <63507739+douglasjrosa@users.noreply.github.com>
2024-04-04 15:26:22 -0300

    Testing page.tsx server components

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,38 +2,34 @@ import FeatureRowsGroup from "@/components/featured-rows-group";
 import ImageHeader from "@/components/image-header";
 import type { Metadata } from 'next';
 import { notFound } from 'next/navigation'
-import { getPost } from "@/lib/utils";
-import { baseUrl } from "@/data/global";
+import { getPost, getPosts } from "@/lib/utils";
+import { Suspense } from "react";
 
-
-
-/*
 export async function generateStaticParams(): Promise<any> {
 	const posts = await getPosts();
 	const slugs = posts.map((post: any) => (post.slug == "home" ? { slug: "" } : { slug: post.slug }));
 	return slugs;
 }
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-	
+
 	const { slug } = params;
 	const post: any | undefined = await getPost(slug);
 	if (post === undefined) notFound();
 	return post.metadata;
 }
-*/
 
 export default async function Page({ params }: { params: { slug: string } }) {
-	/*
+
 	const { slug } = params;
 	const post = await getPost(slug);
 	if (post === undefined) notFound();
 
 	return (
 		<div>
-			<ImageHeader image={post.imageHeader} title={post.title} />
-			<FeatureRowsGroup excerpts={post.excerpts} />
+			<Suspense fallback={<h1>Carregando...</h1>} >
+				<ImageHeader image={post.imageHeader} title={post.title} />
+				<FeatureRowsGroup excerpts={post.excerpts} />
+			</Suspense>
 		</div>
-	)
-	*/
-	return <div className="mt-10">{baseUrl}</div>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,21 @@ import { getPost } from "@/lib/utils";
 import { baseUrl } from "@/data/global";
 import { Suspense } from "react";
 
-/*
 export const metadata: Metadata = {
-  keywords: ["Madeira entalhada", "Produtos de madeira"],
-  authors: [{ name: "Douglas José Rosa", url: "https://github.com/douglasjrosa" }],
-  creator: "Grupo Max Brasil",
-  publisher: "TI - Max Brasil",
+	keywords: ["Madeira entalhada", "Produtos de madeira"],
+	authors: [{ name: "Douglas José Rosa", url: "https://github.com/douglasjrosa" }],
+	creator: "Grupo Max Brasil",
+	publisher: "TI - Max Brasil",
 };
-*/
+
 export default async function Page() {
 	const post = await getPost("home");
 	return (
-		<>
-			<Suspense fallback={<h1>Carregando...</h1>} >
-				<p className="mt-10">{post.title}</p>
-				<p>{post.metadata.title}</p>
-			</Suspense>
-		</>
-	);
-	/*
-	return (
 		<div>
-			<ImageHeader image={post.imageHeader} title={post.title} />
-			<FeatureRowsGroup excerpts={post.excerpts} />
+			<Suspense fallback={<h1>Carregando...</h1>} >
+				<ImageHeader image={post.imageHeader} title={post.title} />
+				<FeatureRowsGroup excerpts={post.excerpts} />
+			</Suspense>
 		</div>
 	)
-	*/
 }
