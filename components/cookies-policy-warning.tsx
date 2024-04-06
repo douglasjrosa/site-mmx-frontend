28f34d0297dec36f0318859ee30ac9af63ccb23c
douglasjrosa <63507739+douglasjrosa@users.noreply.github.com>
2024-04-06 20:22:44 -0300

    Cookies component became lean

--- components/cookies-policy-warning.tsx
+++ components/cookies-policy-warning.tsx
@@ -1,9 +1,18 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
-import DisallowCookiesButton from "./disallow-cookies-button";
-import AllowCookiesButton from "./allow-cookies-button";
-
 
 export async function CookiesPolicyWarning() {
+
+	async function setCookiesDisallowed() {
+		"use server"
+		cookies().delete("cookiesAllowed");
+	}
+
+	async function setCookiesAllowed() {
+		"use server"
+		cookies().set("cookiesAllowed", "true");
+	}
+
 	return (
 		<section>
 			<div className="fixed bottom-0 left-0 w-full bg-black bg-opacity-90 z-50">
@@ -16,8 +25,23 @@ export async function CookiesPolicyWarning() {
 							.</span>
 					</p>
 					<div className="flex space-x-5 items-center justify-end md:w-1/2 lg:w-2/5">
-						<DisallowCookiesButton />
-						<AllowCookiesButton />
+						<form action={setCookiesDisallowed} >
+							<button
+								type="submit"
+								className="px-4 py-1 text-gray-400 hover:bg-red-600 transition duration-300"
+								onClick={setCookiesDisallowed}
+							>
+								<u>Não aceito</u>
+							</button>
+						</form>
+						<form action={setCookiesAllowed} >
+							<button
+								type="submit"
+								className="px-6 py-4 bg-green-700 text-white hover:bg-green-900 transition duration-300"
+							>
+								Aceito
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
